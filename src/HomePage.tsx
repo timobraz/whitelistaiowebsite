5a41ddef4a4862eb8a3dc1300f04253b291d813a
Tim <timofeyobraztsov@gmail.com>
2022-03-29 03:03:17 -0700

    first commit

---
+++ src/HomePage.tsx
@@ -0,0 +1,31 @@
+import "./HomePage.css";
+const HomePage = (props: any) => {
+  const CMTOTAL = props.children;
+  return (
+    <>
+      <div className="centerContainer">
+        <div className="titleContainer">
+          <h1 className="titleText">WHITELISTAIO</h1>
+          <h3 className="secondaryText">Holders Mint</h3>
+        </div>
+        <img src="wlgif.gif" alt="" className="wllogo" />
+        {CMTOTAL}
+        <div className="mediasContainer">
+          <div className="media">
+            <a href="https://discord.gg/gcwRnFc7" className="media" target="_blank">
+              <img src="pixeldiscord.png" alt="discord" className="discordLogo logo" />
+              <span className="mediaText">Discord</span>
+            </a>
+          </div>
+          <div>
+            <a href="https://twitter.com/WhitelistAIO_" className="media" target="_blank">
+              <img src="pixeltwitter.png" alt="twitter" className="twitterLogo logo" />
+              <span className="mediaText">Twitter</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default HomePage;
